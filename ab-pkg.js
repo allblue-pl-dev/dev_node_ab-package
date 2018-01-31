789f81d45df660b2045b2c75adee6affaf588832
SfTd <j.zolcik@allblue.pl>
2018-01-31 12:02:06 +0100

    Development.

--- ab-pkg.js
+++ ab-pkg.js
@@ -1,19 +1,23 @@
 'use strict';
 
 const abPackage = require('ab-package');
+const abPackage_AllBlue = require('ab-package_allblue');
 
 
 abPackage.exec({
     config: {
+        git: 'dev_node_ab-package_test',
         npm: 'ab-package_test',
         bower: 'ab-package_test',
     },
-    package: {
-        name: 'ab-package_test',
-        description: 'Test package for `ab-package` package (a lot of `package` in ' +
-                'this description... package).',
-        author: 'Jakub Zolcik (AllBlue)',
-        gitUri: 'https://github.com/allblue-pl-dev/dev_node_ab-package',
-        keywords: [ 'ab-package-test' ],
-    }
+    package: [
+		abPackage_AllBlue,
+		{
+			name: 'ab-package_test',
+			description: 'Test package for `ab-package` package (a lot of `package` in ' +
+					'this description... package).',
+			gitUri: 'https://github.com/allblue-pl-dev/dev_node_ab-package',
+			keywords: [ 'ab-package-test' ],
+		}
+	],
 });
